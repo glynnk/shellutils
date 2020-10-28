db7c6107a79da77575cf50d002520f3c4f575537
Keith Glynn <glynnk@gmail.com>
2020-10-28 13:02:29 +0000

    initial commit

--- commands/aws-prompt.sh
+++ commands/aws-prompt.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-profile=$(aws configure list 2>/dev/null | egrep "^\s*profile" | awk '{print $2}')
-
-if [[ "$profile" =~ ^[a-z].* ]]; then
-  echo -e "\e[2m<aws:$profile>\e[22m"
+if [ -n "$AWS_PROFILE" ]; then
+  echo -e "\e[2m<aws:$AWS_PROFILE>\e[22m"
 fi
--- plugins/prompt.sh
+++ plugins/prompt.sh
@@ -1,3 +1,6 @@
+case "$TERM" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
 
 if [ "$color_prompt" = yes ]; then
     PS1="\$(aws-prompt.sh)${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$(git-prompt.sh)\$ "
